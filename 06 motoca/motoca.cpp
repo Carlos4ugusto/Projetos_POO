6cbe45f7b3bf261171032c11bab545237f07f9d7
Carlos Augusto <carlos0_23@outlook.com>
2022-02-07 17:42:47 -0300

    POO

--- 06 motoca/motoca.cpp
+++ 06 motoca/motoca.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <sstream>
 
 
 using namespace std;
@@ -13,6 +13,13 @@ struct Pessoa{
         this->idade = idade;
     }
 
+    Pessoa filho(Pessoa pais){
+        Pessoa filho;
+        filho.nome = this->nome + " " + pais.nome;
+        filho.idade = this->idade + pais.idade;
+        return filho;
+    }
+
     friend std::ostream& operator<<(std::ostream& os, const Pessoa& p){
         os << "Nome: " << p.nome << " Idade: " << p.idade;
         return os;
@@ -20,19 +27,75 @@ struct Pessoa{
 };
 
 struct Motoca {
+    Pessoa *pessoa{nullptr};
+    int tempo;
+    int potencia;
 
-        Pessoa pessoa;
-        Motoca() {}
-        void inserirPessoa  (Pessoa pessoa) {
-            this->pessoa = pessoa;
-        }
-    };
-    
-
-    int main() {
-        Motoca motoca;
-        Pessoa pessoa("Joao", 20);
-        motoca.inserirPessoa(pessoa);
-        cout << motoca.pessoa;
-        return 0;
+    Motoca(Pessoa *pessoa = nullptr, int tempo = 0, int potencia = 1){
+        this->pessoa = pessoa;
+        this->tempo = tempo;
+        this->potencia = potencia;
     }
+
+    Motoca(int tempo = 0, int potencia = 1){
+        this->tempo = tempo;
+        this->potencia = potencia;
+    }
+
+    void inserirPessoa(Pessoa *pessoa){
+        this->pessoa = pessoa;
+    }
+
+    Pessoa *removerPessoa(){
+        if(pessoa != nullptr){
+            cout << "Pessoa removida: " << *pessoa << endl;
+            Pessoa *aux = pessoa;
+            pessoa = nullptr;
+            return aux;
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Motoca& m){
+        os << "Tempo: " << m.tempo << " Potencia: " << m.potencia << " Pessoa: " << *m.pessoa;
+        if(m.pessoa == nullptr){
+            os << " Motoca vazia";
+        } else {
+            os << " Motoca cheia";
+        }
+        return os;
+    }
+};
+
+int main(){
+    Motoca m(5, 2);//Motoca vazia com tempo 10 e potencia 2
+
+    while (true){//Enquanto não for digitado 0
+        int init;
+        cout << "Digite 0 para sair ou 1 para inserir uma pessoa: ";
+        cin >> init;
+        if(init == 0){
+            break;
+        }
+
+        cout << "Digite o tempo: ";
+        int tempo;
+        cin >> tempo;
+        cout << "Digite a potencia: ";
+        int potencia;
+        cin >> potencia;
+        cout << "Digite o nome: ";
+        std::string nome;
+        cin >> nome;
+        cout << "Digite a idade: ";
+        int idade;
+        cin >> idade;
+
+        Pessoa p(nome, idade);//nova pessoa com os dados digitados
+        m.inserirPessoa(&p);//insere a pessoa na motoca e retorna a pessoa removida
+        cout << "Motoca: " << m << endl;
+        cout << "Motoca: " << m.removerPessoa() << endl;
+
+    }
+    
+    return 0;
+}
